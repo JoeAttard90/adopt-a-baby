348060d65189cf9bce668b15b848bd9aab3cb485
JoeAttard90 <attard999@gmail.com>
2022-04-02 17:53:32 +0100

    updated package and favicon as well as urls

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -5,8 +5,8 @@ import './content.css'
 const Content = () => {
   return (
     <>
-        <a href="/"><img class='main-img' src={mainImage} alt="adopt-a-baby"></img></a>
-        <a href="https://opensea.io/AdoptABaby" target="_blank" rel="noreferrer" class="welcome-link">
+        <a href="https://discord.gg/VxU4APxEHq"><img class='main-img' src={mainImage} alt="adopt-a-baby"></img></a>
+        <a href="https://www.instagram.com/adoptababynft/" target="_blank" rel="noreferrer" class="welcome-link">
             <h2 class="welcome">
                 -
                 Adopt a Baby NFTs <u>Coming Soon!</u>
--- src/components/Footer/CustomFooter.js
+++ src/components/Footer/CustomFooter.js
@@ -14,7 +14,7 @@ const CustomFooter = () => {
           <ul>
             <li><a href="https://www.instagram.com/adoptababynft/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="icons"/></a></li>
             <li><a href="https://twitter.com/NftAdoptABaby" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} className="icons"/></a></li>
-            <li><a href="/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord} className="icons"/></a></li>
+            <li><a href="https://discord.gg/VxU4APxEHq" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord} className="icons"/></a></li>
             {/*<li><a href="https://opensea.io/AdoptABaby" target="_blank" rel="noreferrer"><img class='oceansea-logo' src={openSea} alt="adopt-a-baby"></img></a></li>*/}
           </ul>
         </div>
